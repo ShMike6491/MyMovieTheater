c8b9585f55d5753523efa2bb7b6654d31ef0daba
Mike Shilovski <ShMike6491@gmail.com>
2021-04-19 20:48:43 +0300

    start over

---
+++ app/src/main/java/com/mymovietheater/ui/home/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.mymovietheater.ui.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+}
